66329aff1b369c970c1d4eb03952dbe56003cd07
mrnkslv <mrnkslv@gmail.com>
2025-10-20 15:51:21 +0300

    cli: return standart start

--- scripts/tilt-gcp-up.sh
+++ scripts/tilt-gcp-up.sh
@@ -8,7 +8,8 @@ if [[ "$EUID" -eq 0 ]]; then
   exit 1
 fi
 
-EXT_IP=$(curl ipinfo.io/ip)
+EXT_IP=$(curl -s -H "Metadata-Flavor: Google" \
+  "http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip")
 
 IFACE=$(ip route | awk '/default/ { print $5 }')
 
